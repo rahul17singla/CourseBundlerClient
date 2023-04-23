6752901160c47d1746393881ed6cbb07c7a5bb11
Rahul Singla <rahulsingla1710@gmail.com>
2023-04-23 12:46:48 +0530

    new commit

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,43 +1,47 @@
-import { Box, Heading, HStack, Stack, VStack } from '@chakra-ui/react';
-import React from 'react';
-import {
-  TiSocialYoutubeCircular,
-  TiSocialInstagramCircular,
-} from 'react-icons/ti';
-import { DiGithub } from 'react-icons/di';
+import { Box, Heading, HStack, Stack, VStack } from "@chakra-ui/react";
+import React from "react";
+import { TiSocialInstagramCircular } from "react-icons/ti";
+import { FcBusinessman } from "react-icons/fc";
+import { DiGithub } from "react-icons/di";
 const Footer = () => {
-  return (
-    <Box padding={'4'} bg="blackAlpha.900" minH={'10vh'}>
-      <Stack direction={['column', 'row']}>
-        <VStack alignItems={['center', 'flex-start']} width="full">
-          <Heading children="All Rights Reserved" color={'white'} />
-          <Heading
-            fontFamily={'body'}
-            size="sm"
-            children="@6 Pack Programmer"
-            color={'yellow.400'}
-          />
-        </VStack>
+    return (
+        <Box padding={"4"} bg="blackAlpha.900" minH={"10vh"}>
+            <Stack direction={["column", "row"]}>
+                <VStack alignItems={["center", "flex-start"]} width="full">
+                    <Heading children="All Rights Reserved" color={"white"} />
+                    <Heading
+                        fontFamily={"body"}
+                        size="sm"
+                        children="@Rahul Singla"
+                        color={"yellow.400"}
+                    />
+                </VStack>
 
-        <HStack
-          spacing={['2', '10']}
-          justifyContent="center"
-          color={'white'}
-          fontSize="50"
-        >
-          <a href="https://youtube.com/6packprogrammer" target={'blank'}>
-            <TiSocialYoutubeCircular />
-          </a>
-          <a href="https://instagram.com/meabhisingh" target={'blank'}>
-            <TiSocialInstagramCircular />
-          </a>
-          <a href="https://github.com/meabhisingh" target={'blank'}>
-            <DiGithub />
-          </a>
-        </HStack>
-      </Stack>
-    </Box>
-  );
+                <HStack
+                    spacing={["2", "10"]}
+                    justifyContent="center"
+                    color={"white"}
+                    fontSize="50"
+                >
+                    <a href="https://rsingla17.netlify.app/" target={"_blank"}>
+                        <FcBusinessman />
+                    </a>
+                    <a
+                        href="https://www.instagram.com/rahul17singla/"
+                        target={"_blank"}
+                    >
+                        <TiSocialInstagramCircular />
+                    </a>
+                    <a
+                        href="https://github.com/rahul17singla"
+                        target={"_blank"}
+                    >
+                        <DiGithub />
+                    </a>
+                </HStack>
+            </Stack>
+        </Box>
+    );
 };
 
 export default Footer;
